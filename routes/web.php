d34cd824b91d76ffcc59ea0fd63114107fb77faa
Thiago Albuquerque de Paula <thiago@tenil.com.br>
2022-04-06 00:53:55 -0300

    Fazendo aula EspecializaTi - Aula 8

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\{
+    UserController
+};
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('user.show');
